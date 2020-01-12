dc8215b172acd5761a4f1f24a25593b4186a88d4
jolly-assembler-1925 <jnzhang622@gmail.com>
2020-01-12 17:01:35 +0000

    Automatically backed up by Learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,5 +1,5 @@
 def greeting
-  puts "Hello Ruby Programmer"
+  puts "hello ruby programmer"
 	puts "Please enter your name so that we can greet you more personally:"
   name = gets.strip
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
